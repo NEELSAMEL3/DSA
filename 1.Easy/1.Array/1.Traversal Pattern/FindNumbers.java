3f6aafa60e4a044201b73b84b082c061b317105c
NEELSAMELL369 <neelsamel369@gmail.com>
2026-02-21 13:34:37 +0530

    DSA folder structure

---
+++ 1.Easy/1.Array/1.Traversal Pattern/FindNumbers.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class FindNumbers {
+
+    public static int findNumbers(int[] nums) {
+        int count = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            int contains = String.valueOf(nums[i]).length();
+
+            if(contains % 2 == 0){
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+        System.out.print("Enter elements: ");
+        
+        for(int i = 0; i < n; i++){
+            nums[i] = sc.nextInt();
+        }
+
+        int result = findNumbers(nums);
+
+        System.out.println("Even digit numbers count: " + result);
+    }
+}
---
+++ 1.Easy/1.Array/1.Traversal Pattern/numberOfEmployeesWhoMetTarget.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int count = 0;
+
+        for (int i = 0; i < hours.length; i++) {
+            if (hours[i] >= target) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
+
+public class numberOfEmployeesWhoMetTarget {
+    public static void main(String[] args) {
+
+        Solution obj = new Solution();
+
+        int[] hours = {0, 1, 2, 3, 4};
+        int target = 2;
+
+        int result = obj.numberOfEmployeesWhoMetTarget(hours, target);
+
+        System.out.println(result);
+    }
+}
